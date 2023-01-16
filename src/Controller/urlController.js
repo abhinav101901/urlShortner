7d2b57216f946a3c64e16e8314acdbcaa5375eb7
abhinav101901 <abhinav8299233838@gmail.com>
2023-01-16 22:13:59 +0530

    project

--- src/Controller/urlController.js
+++ src/Controller/urlController.js
@@ -2,9 +2,8 @@ const UrlModel=require("../model/urlModel");
 const shortid=require("shortid");
 const axios=require("axios")
 
-
+//---------------------------------------Creating urlData--------------------------------------------------------
 const createShortUrl=async function(req,res){
-
 
     let obj={};
     let data =req.body;
@@ -20,28 +19,39 @@ await axios.get(data.longUrl)
 })
 .catch((error)=>{})
 
-if(urlfound==false) return res.status(400).send({status:false,message:"Invalid Link"})
-
-
-
+if(urlfound==false) return res.status(400).send({status:false,message:"Invalid long Url"})
+ 
     let urlCode= shortid.generate();
+    let urlCodePresent = await UrlModel.findOne({urlCode:urlCode})
+    if(urlCodePresent) return res.status(400).send({status:false,message:"Url code is already present"})
+
     let shortUrl= "http://localhost:3000/"+urlCode;
+    let shortUrlPresent = await UrlModel.findOne({shortUrl:shortUrl})
+    if(shortUrlPresent) return res.status(400).send({status:false,message:"Short Url is already present"})
+
     obj.longUrl=data.longUrl;
     obj.shortUrl=shortUrl;
     obj.urlCode=urlCode;
 
-   
     let created = await UrlModel.create(obj)
     return res.status(201).send({status:true,data:created})
 }
 
+//---------------------------------------------- Get urlData -----------------------------------------------------------------------------------
+
 const redirectUrl=async function(req,res){
     let urlCode=req.params.urlCode;
+    if(!urlCode) return res.status(400).send({status:false,message:"Please enter the Url Code"})
+
+    if(!shortid.isValid(urlCode)) return res.status(400).send({status:false,message:"Please enter correct Url code"})
+
+    let checkUrlCode = await UrlModel.findOne({urlCode:urlCode})
+    if(!checkUrlCode) return res.status(404).send({status:false,messgae:"Url Code not found"})
+
     let getUrl= await UrlModel.findOne({urlCode:urlCode}).select({longUrl:1,_id:0})
     return res.status(200).send({status:true,data:getUrl})
 }
 
 
 
-module.exports.createShortUrl=createShortUrl
-module.exports.redirectUrl=redirectUrl
+module.exports={createShortUrl,redirectUrl}
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,12 +2,15 @@ const express=require("express");
 const router=express.Router();
 const UrlController=require("../Controller/urlController")
 
-
-
-
+//------------------ Creating Url Data --------------------------------------
 router.post("/url/shorten",UrlController.createShortUrl)
+
+//-----------------------Get url-----------------------------------------------
 router.get("/:urlCode",UrlController.redirectUrl)
-router.all("/*",function(req,res){
+
+//----------------------------Default----------------------------------------
+router.all("/**",function(req,res){
     return res.status(400).send({status:false,message:"Invalid URL"})
 })
+
 module.exports=router;
