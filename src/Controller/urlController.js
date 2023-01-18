15705625816de5683d703c44efd994e1cdcb5f1e
abhinav101901 <abhinav8299233838@gmail.com>
2023-01-18 19:31:27 +0530

    add

--- src/Controller/urlController.js
+++ src/Controller/urlController.js
@@ -1,7 +1,24 @@
 const UrlModel=require("../model/urlModel");
 const shortid=require("shortid");
 const axios=require("axios")
+const redis = require("redis")
+const {promisify} = require("util")
 
+const redisClient = redis.createClient(
+    12528,
+    "redis-12528.c305.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
+  );
+  redisClient.auth("0xa8wqjSDVnbenDx487mH0PlMMNoWExy", function (err) {
+    if (err) throw err;
+  });
+  
+  redisClient.on("connect", async function () {
+    console.log("Connected to Redis..");
+  });
+
+const SETEX_ASYNC = promisify(redisClient.SETEX).bind(redisClient);
+const GETEX_ASYNC = promisify(redisClient.GETEX).bind(redisClient);
 //---------------------------------------Creating urlData--------------------------------------------------------
 const createShortUrl=async function(req,res){
     try{
@@ -22,10 +39,16 @@ const createShortUrl=async function(req,res){
     .catch((error)=>{})
 
     if(urlfound==false) return res.status(400).send({status:false,message:"Invalid long Url"})
+     
+    //............check in cache-memory...................
+    let cacheData = await GETEX_ASYNC(`${data.longUrl}`)//https://
+    if(cacheData) return res.status(200).send({status:true,data:cacheData})
 
+    //............ Check in DataBase................
     let longUrlPresent = await UrlModel.findOne({longUrl:data.longUrl}).select({_id:0,createdAt:0,updatedAt:0,__v:0})
     if(longUrlPresent) return res.status(200).send({status:true,data:longUrlPresent})
 
+    //..............Generate....................................
     let urlCode= shortid.generate().toLowerCase();//fdjgfdnsg7 --->fDjGfDnsg7-->fdjgfdnsg7
 
     let urlCodePresent = await UrlModel.findOne({urlCode:urlCode})
@@ -38,7 +61,13 @@ const createShortUrl=async function(req,res){
     obj.urlCode=urlCode;
 
     let created = await UrlModel.create(obj)
+    //.................................................................
+
+    //.........Take data and store in cache-memory....................
     let result= await UrlModel.findById(created._id).select({longUrl:1,shortUrl:1,urlCode:1,_id:0})
+    await SETEX_ASYNC(`${created.longUrl}`, 120, JSON.stringify(result))
+    //......................................................................
+
     return res.status(201).send({status:true,data:result})
 }
     catch(error){
@@ -51,19 +80,24 @@ const createShortUrl=async function(req,res){
 const redirectUrl=async function(req,res){
     try{
         let urlCode=req.params.urlCode;
-    if(!urlCode) return res.status(400).send({status:false,message:"Please enter the Url Code"})
 
     if(!shortid.isValid(urlCode)) return res.status(400).send({status:false,message:"Please enter correct Url code"})
+    
+    //...............Check UrlCode in cache-memory or Not....................................................................
+    let cacheData = await GETEX_ASYNC(`${urlCode}`)
+    let obj= JSON.parse(cacheData)
+    if(obj) return res.status(302).redirect(obj.longUrl)
 
+    //..............Check Url code in DataBase .................................................
     let checkUrlCode = await UrlModel.findOne({urlCode:urlCode}).select({longUrl:1,_id:0})
     if(!checkUrlCode) return res.status(404).send({status:false,message:"Url Code not found"})
-
+     
     let Url = checkUrlCode.longUrl 
-    // h.get('*',function(req,res){  
-    //     res.status(302).redirect(Url)
-    // })
+    
+    //...................
+    await SETEX_ASYNC(`${urlCode}`, 120, JSON.stringify(checkUrlCode))
 
-    return res.redirect(Url)
+    return res.status(302).redirect(Url)
 }
     catch(error){
         return res.status(500).send({status:false,Error:error.message})
